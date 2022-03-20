c0188fe73c3580ed0b72b927259fdeed1b86948a
IrinaVerbitskaja <mgmt-m@yandex.ru>
2022-03-20 15:30:07 +0300

    модуль 7 задание 2 доправила

--- js/open-close.js
+++ js/open-close.js
@@ -1,21 +1,21 @@
 const userFormCard = document.querySelector('.ad-form');
-const userFormCardElement = userFormCard.querySelectorAll('fieldset');
+const userFormCardElements = userFormCard.querySelectorAll('fieldset');
 const filterForm = document.querySelector('.map__filters');
-const filterFormElement = filterForm.querySelectorAll('select');
+const filterFormElements = filterForm.querySelectorAll('select');
 const filterFormElementCheck = filterForm.querySelector('.map__features');
 
 
 userFormCard.classList.add('ad-form--disabled');
 filterForm.classList.add('map__filters--disabled');
-userFormCardElement.setAttribute('disabled', true);
-filterFormElement.setAttribute('disabled', true);
+userFormCardElements.forEach((userFormCardElement) => {userFormCardElement.setAttribute('disabled', true);});
+filterFormElements.forEach((filterFormElement) => {filterFormElement.setAttribute('disabled', true);});
 filterFormElementCheck.setAttribute('disabled', true);
 
 function openPage () {
   userFormCard.classList.remove('ad-form--disabled');
   filterForm.classList.remove('map__filters--disabled');
-  userFormCardElement.removeAttribute('disabled');
-  filterFormElement.removeAttribute('disabled');
+  userFormCardElements.forEach((userFormCardElement) => {userFormCardElement.removeAttribute('disabled');});
+  filterFormElements.forEach((filterFormElement) => {filterFormElement.removeAttribute('disabled');});
   filterFormElementCheck.removeAttribute('disabled');
 }
 
